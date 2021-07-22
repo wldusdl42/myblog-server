3150d5b64183624825a1464f8fd6205d3c8a118e
jiyKim <wldusdl34@gmail.com>
2021-07-22 14:42:53 +0900

    수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,9 @@ module.exports = {
     "apps" : [{
         "name": "server",
         "script": "./dist/main.js",
+        "exec_mode": "cluster",
         "instances": 2, // 앱 인스턴스의 수
         "watch": true,
-        "error_file": "./errors.log",
-        "env": { // 환경변수. 모든 배포 환경에서 공통으로 사용한다.
-            "NODE_ENV": 'development',
-          },
-        "env_production" : {
-            "NODE_ENV": "production"
-        }
+        "error_file": "./errors.log"
     }]
 }
