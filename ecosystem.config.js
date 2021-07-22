2462eaa038f44406d90d00eba4b44ccf254aacf4
jiyKim <wldusdl34@gmail.com>
2021-07-22 15:08:52 +0900

    수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     "apps" : [{
         "name": "server",
-        "script": "./dist/main.js"
+        "script": "./dist/main.js",
+        "exec_mode": "cluster",
+        "instances": 2, // 앱 인스턴스의 수
+        "watch": true,
+        "error_file": "./errors.log"
     }]
 }
